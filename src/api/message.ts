c50ade5bc3073aac5d142fef39bbccb9692421e0
tiagohm <tiago.henrique.cco@gmail.com>
2025-09-25 07:45:59 -0300

    implement api and web

--- src/api/message.ts
+++ src/api/message.ts
@@ -6,6 +6,7 @@ export interface Messager {
 
 export class WebSocketMessageManager {
 	private readonly sockets = new Set<Messager>()
+	private readonly events = new Map<string, unknown>()
 
 	open(socket: Messager) {
 		if (!this.sockets.has(socket)) {
@@ -14,23 +15,52 @@ export class WebSocketMessageManager {
 		}
 	}
 
-	message(socket: Messager, message: string | Buffer) {
-		//
+	message(socket: Messager, message: unknown) {
+		if (message === 'RESEND') {
+			this.resend(undefined, socket)
+			return
+		}
 	}
 
 	close(socket: Messager, code: number, reason: string) {
 		if (this.sockets.has(socket)) {
-			console.info('web socket closed: ', code, reason)
+			console.info('web socket closed:', code, reason)
 			this.sockets.delete(socket)
 		}
 	}
 
-	send<T = unknown>(type: string, message: T) {
+	send<T = unknown>(type: string, message: T, socket?: Messager) {
 		bus.emit(type, message)
 
 		if (this.sockets.size) {
 			const data = `${type}@${JSON.stringify(message)}`
-			this.sockets.forEach((socket) => socket.sendText(data))
+
+			if (socket) {
+				socket.sendText(data)
+			} else {
+				this.sockets.forEach((socket) => socket.sendText(data))
+
+				if (!type.endsWith(':add') && !type.endsWith(':remove')) {
+					this.events.set(type, message)
+				}
+			}
 		}
+	}
+
+	resend(type?: string, socket?: Messager) {
+		if (!this.events.size) return
+
+		console.info('web socket resending')
+
+		if (!type) {
+			this.events.forEach((message, type) => this.send(type, message, socket))
+		} else {
+			const message = this.events.get(type)
+			message !== undefined && this.send(type, message, socket)
+		}
+	}
+
+	event<T = unknown>(type: string) {
+		return this.events.get(type) as T | undefined
 	}
 }
--- src/web/molecules/ws.ts
+++ src/web/molecules/ws.ts
@@ -1,61 +1,100 @@
-import { molecule } from 'bunshi'
+import { molecule, onMount } from 'bunshi'
 import bus from 'src/shared/bus'
 import type { DeviceAdded } from 'src/shared/types'
 import { NotificationMolecule } from './notification'
+
+let ws: WebSocket | undefined
 
 export const WebSocketMolecule = molecule((m) => {
 	const notification = m(NotificationMolecule)
 
 	const uri = localStorage.getItem('api.uri') || `${location.protocol}//${location.host}`
-	const ws = new WebSocket(`${uri}/ws`)
 
-	ws.addEventListener('open', () => console.info('web socket open'))
-	ws.addEventListener('close', (e) => console.info('web socket close', e))
-
-	ws.addEventListener('message', (message) => {
-		if (!bus.hasSubscribers()) return
-
-		const content = message.data as string
-		const index = content.indexOf('@')
-
-		if (index === -1) {
-			console.warn('invalid web socket message:', message.data)
+	function create() {
+		if (ws && (ws.readyState === WebSocket.OPEN || ws.readyState === WebSocket.CONNECTING)) {
 			return
 		}
 
-		const key = content.slice(0, index)
-		const text = content.slice(index + 1)
-		const data = JSON.parse(text)
+		ws = new WebSocket(`${uri}/ws`)
 
-		switch (key) {
-			case 'camera:add':
-			case 'camera:remove':
-			case 'mount:add':
-			case 'mount:remove':
-			case 'focuser:add':
-			case 'focuser:remove':
-			case 'wheel:add':
-			case 'wheel:remove':
-			case 'guideOutput:add':
-			case 'guideOutput:remove':
-			case 'thermometer:add':
-			case 'thermometer:remove':
-			case 'cover:add':
-			case 'cover:remove':
-			case 'flatPanel:add':
-			case 'flatPanel:remove':
-			case 'dewHeater:add':
-			case 'dewHeater:remove':
-				bus.emit(key, (data as DeviceAdded).device)
-				break
-			case 'notification':
-				notification.send(data)
-				break
-			default:
-				bus.emit(key, data)
-				break
+		ws.addEventListener('open', () => {
+			send('RESEND')
+			console.info('web socket open')
+		})
+
+		ws.addEventListener('close', (e) => {
+			console.info('web socket close', e)
+		})
+
+		ws.addEventListener('message', (message) => {
+			if (!bus.hasSubscribers()) return
+
+			const content = message.data as string
+			const index = content.indexOf('@')
+
+			if (index === -1) {
+				console.warn('invalid web socket message:', message.data)
+				return
+			}
+
+			const key = content.slice(0, index)
+			const text = content.slice(index + 1)
+			const data = JSON.parse(text)
+
+			switch (key) {
+				case 'camera:add':
+				case 'camera:remove':
+				case 'mount:add':
+				case 'mount:remove':
+				case 'focuser:add':
+				case 'focuser:remove':
+				case 'wheel:add':
+				case 'wheel:remove':
+				case 'guideOutput:add':
+				case 'guideOutput:remove':
+				case 'thermometer:add':
+				case 'thermometer:remove':
+				case 'cover:add':
+				case 'cover:remove':
+				case 'flatPanel:add':
+				case 'flatPanel:remove':
+				case 'dewHeater:add':
+				case 'dewHeater:remove':
+					bus.emit(key, (data as DeviceAdded).device)
+					break
+				case 'notification':
+					notification.send(data)
+					break
+				default:
+					bus.emit(key, data)
+					break
+			}
+		})
+	}
+
+	function send(data: string | ArrayBufferLike | Blob | ArrayBufferView<ArrayBufferLike>) {
+		ws?.send(data)
+	}
+
+	function close() {
+		ws?.close()
+		ws = undefined
+	}
+
+	onMount(() => {
+		const timer = setInterval(() => {
+			if (ws && ws.readyState === WebSocket.CLOSED) {
+				console.info('reconnecting web socket...')
+				create()
+			}
+		}, 5000)
+
+		create()
+
+		return () => {
+			clearInterval(timer)
 		}
 	})
 
-	return { ws } as const
+	return { send, close } as const
 })

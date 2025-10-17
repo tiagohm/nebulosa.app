2b7fae166690f191dff2dc3495dc6904b6c16af3
tiagohm <tiago.henrique.cco@gmail.com>
2025-10-16 22:25:21 -0300

    support build on windows

--- build.ts
+++ build.ts
@@ -1,10 +1,20 @@
 import { build } from 'bun'
+import { version } from 'package.json'
 
 await build({
 	entrypoints: ['./main.ts'],
 	minify: true,
 	target: 'bun',
 	compile: {
-		outfile: 'nebulosa.exe',
+		outfile: process.platform === 'win32' ? 'nebulosa.exe' : 'nebulosa.out',
+		windows: {
+			title: 'Nebulosa',
+			description: 'The complete integrated solution for all of your astronomical imaging needs.',
+			publisher: 'Tiago Melo',
+			icon: 'src/web/assets/nebulosa.ico',
+			copyright: '© 2025 Tiago Melo',
+			hideConsole: false,
+			version,
+		},
 	},
 })
